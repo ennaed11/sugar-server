3b3edbcf8de0c8ed97e6e0956fc15ee8651a5fa8
Michael Pio <mfmayol@up.edu.ph>
2018-11-17 07:08:10 +0800

    fix trailling comma

--- mailer.py
+++ mailer.py
@@ -4,7 +4,7 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate, 
+from email.utils import COMMASPACE, formatdate
 
 class Mailer:
     def __init__(self, username, password, url, port):
